c8321e8b9a515e2ff93a2668e0562336b30a3ce5
Youssef <84786954+youssefhassiss@users.noreply.github.com>
2021-12-14 21:49:36 +0100

    Add files via upload

---
+++ complexe.c
@@ -0,0 +1,29 @@
+#include<math.h>
+#include "complexe.h"
+
+Complexe nouveauComplexe(double r, double i)
+{
+Complexe z;
+z.re=r;
+z.im=i;
+return z;
+}
+
+Complexe plus(Complexe z1,Complexe z2)
+{
+return nouveauComplexe(z1.re+z2.re,z1.im+z2.im);
+}
+
+Complexe moins(Complexe z1,Complexe z2)
+{
+return nouveauComplexe(z1.re-z2.re,z1.im-z2.im);
+}
+
+double module(Complexe z)
+{
+return sqrt(z.re*z.re + z.im*z.im);
+}
+double argument(Complexe z)
+{
+return atan(z.im / z.re);
+}
---
+++ useComplexe.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "complexe.h"
+
+int main()
+{
+Complexe z1, z2, z3;
+
+z1 = nouveauComplexe(1.,0.);
+z2 = nouveauComplexe(0.,1.);
+z3 = plus(z1,z2);
+
+printf("Module de Z3 : %f\n ",module(z3));
+printf("Argument de z3 : %f\n",argument(z3));
+
+return EXIT_SUCCESS;
+
+}
